5b2c732284817a43970fe7db0eafb474fd1eba8a
LucasS1lv4 <lucas.lukas3245@gmail.com>
2023-12-01 18:00:17 -0300

    estudos

--- 20Questão.js
+++ 20Questão.js
@@ -1,15 +1,24 @@
 /* Escreva um algoritmo que percorre um vetor de inteiros e defina o maior e menor valor dentro do vetor.*/
 
 function INTC(vetor){
-let maior = 0
+    let maior
+    let menor
 for(let i = 0; i <= vetor.length; i++){
-
-    if(maior <= vetor[i]){
+    if(maior == undefined && menor== undefined){
+        maior = vetor[i] 
+        menor= vetor[i]
+    } else{
+    if(vetor[i] > maior ){
         
     maior= vetor[i]
+
+    }if(vetor[i] < menor){
+        menor= vetor[i]
     }
 }
+}   
 console.log(`O maior numero deste array é: ${maior}`)
+console.log(`O menor numero deste array é: ${menor}`)
 }
 
-INTC([24,17,20,23,27,22,25,11,13,15])
+INTC([1,24,17,20,23,27,22,25,11,13,15])
