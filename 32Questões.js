c55745961cdf0399934d073dfa799bee42cf7096
LucasS1lv4 <lucas.lukas3245@gmail.com>
2023-12-15 20:54:13 -0300

    Estudos-Js

---
+++ 32Questões.js
@@ -0,0 +1,11 @@
+/*Crie uma função que receba dois números e retorne se o primeiro é maior do que o segundo.*/
+
+function MaiorOuIgual(numero1,numero2){
+    if(numero1 >= numero2){
+        return numero1 >= numero2
+    }else{
+        return numero1 >= numero2
+    }
+}
+
+console.log(MaiorOuIgual(2,20))
